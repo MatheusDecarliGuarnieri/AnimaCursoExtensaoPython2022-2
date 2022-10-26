ba0f90db36c630880406f8ba78ddf96b16d12634
Matheus Decarli <116276080+MatheusDecarliGuarnieri@users.noreply.github.com>
2022-10-26 14:26:01 +0000

    primeiro print de variáveis

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-# Meu primeiro projeto python!!!
-
 #print() = comando de saído
 print("Alô mundo!")
+
+#Guardar string 
+nome = "Filipe Mello"
+idade = 18
+
+#Exibir nome dentro da variável
+print("Meu nome é", nome,"Tenho",idade,"anos")
+
 
 '''
 Comentario em bloco

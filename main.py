0e38f16ad0af1f16b5c295c8170b70b2f806cee2
Matheus Decarli <116276080+MatheusDecarliGuarnieri@users.noreply.github.com>
2022-10-27 00:05:35 +0000

    Começo de Estrutura condicional

--- main.py
+++ main.py
@@ -13,3 +13,13 @@ print ("Olá seu nome é {} e sua idade é {}".format(nome, idade))
 #e se eu quisse mostrar o dobro da idade informada?
 dobro = idade * 2
 print ("o dobro da idade informada é {}".format(dobro))
+
+# estrutura condicional - o famoso "SE" (if)
+#Se a pessoa for maior de idade , mostre "Você é maior de idade, ótimo! Já pode beber ou dirigir"
+
+#if (idade >= 18):
+if idade >= 18:
+  print("Você é maior de idade, ótimo! Já pode beber ou dirigir")
+
+else:
+  print("Você é menor de idade, ainda falta tempo para poder beber ou dirigir")

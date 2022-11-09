4468780161ae2b09d380d90b7c49cd1997399c80
Matheus Decarli <116276080+MatheusDecarliGuarnieri@users.noreply.github.com>
2022-11-09 23:22:25 +0000

    Exemplo de while e Lista com while

---
+++ Aula2-Parte3.py
@@ -0,0 +1,28 @@
+#Exemplo de Laço
+
+# se eu quisesse exibir números de 1 a 10?
+
+#Maneira errada
+print("Maneira errada")
+print("")
+
+print("1")
+print("2")
+print("3")
+print("4")
+print("5")
+print("6")
+print("7")
+print("8")
+print("9")
+print("10")
+print("")
+
+#Maneira correta
+print("Maneira Correta")
+print("")
+contador = 1
+
+while (contador <= 10):
+  print(contador)
+  contador = contador + 1
---
+++ Aula3.py
@@ -0,0 +1,53 @@
+print ("Início da aula 3 (09/11/2022) ")
+
+contador = 1
+
+#Exibir de 1 até 10 repedidamente 
+while(contador <= 10):
+  print(contador)
+  contador = contador + 1 #ou contador += 1
+
+#Laço for(python for = for each)
+fruta = "morango"
+print(fruta)
+
+fruta1 = "morango"
+fruta2 = "laranja"
+fruta3 = "pêra"
+
+#Lista
+frutas = ["morango", "laranja","pêra" ]
+
+#Mostra todas
+print (frutas)
+
+#Quero exibir apenas a 3 fruta (pêra)
+print (frutas[2])
+
+#Exibir quantas frutas têm na minha tela?
+print(len(frutas)) # length = tamanho
+
+#Quero incluir uma fruta nova
+frutas.append("manga")
+
+print(len(frutas)) # length = tamanho
+print (frutas)
+
+print(frutas[0])
+print(frutas[1])
+print(frutas[2])
+print(frutas[3])
+#print(frutas[4])
+
+#i = 0
+#while(i<4):
+#  print(frutas[i])
+#  i += 1
+  
+print ("Exemplo das frutas com while")
+frutas.append("uva")
+
+i = 0 # (i de index)
+while(i<len(frutas)):
+  print(frutas[i])
+  i += 1
